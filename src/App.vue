ef861ee72ff1e3ba28241f6f50d30cf26e1e271f
Amir Hassan <amirhassan.hamzan@gmail.com>
2023-07-15 21:02:13 +0800

    Update main title under maintainance

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Amir Vue Todo App!" />
+      <HelloWorld msg="Ooops! The website is under maintenance." />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
