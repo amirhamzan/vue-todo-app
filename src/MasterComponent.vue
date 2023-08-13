8bff6dd083b1e3a84518cfa51d831c4d2a2207ef
Amir Hassan <amirhassan.hamzan@gmail.com>
2023-08-14 07:43:24 +0800

    Master component to code todo app

---
+++ src/MasterComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <main>
+    <section class="container top-section">
+      <h1 class="heading-primary">Todo List</h1>
+      <section class="section-add-list">
+        <input v-model="inputTask" class="add-list" type="text" id="fname" name="fname" />
+        <button @click="addTask()" type="submit">Add Task</button>
+      </section>
+    </section>
+    <section class="container bottom-section">
+      <div v-for="task in listTaskArray" :key="task">
+        <div class="task-detail">
+          <div>
+            <button style="margin-right: 10px;">done</button>
+            <span>
+              {{ task }}
+            </span>
+          </div>
+          <button>delete</button>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const listTaskArray = ref([])
+const inputTask = ref('')
+
+function addTask() {
+  console.log(inputTask.value)
+  listTaskArray.value.push(inputTask.value)
+  inputTask.value = ''
+  // console.log(inputTask.value);
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import './assets/main.css'
+import './assets/style.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
+import App from './MasterComponent.vue'
 import router from './router'
 
 import 'vuetify/styles'
@@ -20,6 +20,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(vuetify)
+// app.use(vuetify)
 
 app.mount('#app')
